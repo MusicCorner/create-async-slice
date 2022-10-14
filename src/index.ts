406c87e5c6ff4b758483094eda17d6a587498e9c
Alex Farshatov <aleksei.farshatov@toptal.com>
2022-10-14 16:48:15 +0700

    making index file

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './createAsyncListSlice';
+export * from './createAsyncSlice';
+
+// import { DefaultListErrorPayload, DefaultListPayload, DefaultListSuccessPayload } from './createAsyncListSlice';
+// import { AsyncState, _AsyncState, _CreateSliceOptions } from './createAsyncSlice';
+
+// export DefaultListErrorPayload = ;
